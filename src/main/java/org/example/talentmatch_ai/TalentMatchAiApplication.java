a85ac72685b1076cb75c04a1f43c1114f9d1abef
KFLandry <wilfried-landry.kankeufokou@worldline.com>
2026-03-06 10:13:50 +0100

    First commit

---
+++ src/main/java/org/example/talentmatch_ai/TalentMatchAiApplication.java
@@ -0,0 +1,13 @@
+package org.example.talentmatch_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentMatchAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalentMatchAiApplication.class, args);
+    }
+
+}
