8a87b3869c008bfb7a1809113822dbad4bf0e594
KFLandry <wilfried-landry.kankeufokou@worldline.com>
2026-03-07 20:57:36 +0100

    feat: Add @Repository annotation to JobOfferRepo interface

--- src/main/java/org/example/talentmatch_ai/repository/JobOfferRepo.java
+++ src/main/java/org/example/talentmatch_ai/repository/JobOfferRepo.java
@@ -2,8 +2,10 @@ package org.example.talentmatch_ai.repository;
 
 import org.example.talentmatch_ai.model.JobOffer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface JobOfferRepo  extends JpaRepository<JobOffer, UUID> {
 }
